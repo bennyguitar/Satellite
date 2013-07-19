af31862ce552a081ce6c61674687991c6b962209
BenGordon <brgordon@ua.edu>
2013-07-19 09:10:12 -0500

    Initial Commit

---
+++ Satellite/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Satellite
+//
+//  Created by Benjamin Gordon on 7/19/13.
+//  Copyright (c) 2013 Benjamin Gordon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
